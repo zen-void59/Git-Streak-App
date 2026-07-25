32f53378c075742d6f397c0ab5e89b0e65698195
zen-void59 <ayushcodes78@gmail.com>
2026-07-25 22:57:54 +0530

    done with the habit tracker page

--- lib/habits_page.dart
+++ lib/habits_page.dart
@@ -1,6 +1,39 @@
 import "package:flutter/material.dart";
-class HabitsPage extends StatelessWidget {
+
+class HabitsPage extends StatefulWidget {
   const HabitsPage({super.key});
+
+  @override
+  State<HabitsPage> createState() => _HabitsPageState();
+}
+
+class _HabitsPageState extends State<HabitsPage> {
+  bool isCompleted = false;
+  final TextEditingController controller = TextEditingController();
+
+  List<Map<String, dynamic>> habits = [
+    {
+      "name": "Coding",
+      "days": 124,
+      "completed": true,
+      "selected": false,
+      "icon": Icons.computer,
+    },
+    {
+      "name": "Read 20 min",
+      "days": 14,
+      "completed": false,
+      "selected": false,
+      "icon": Icons.menu_book,
+    },
+    {
+      "name": "Workout",
+      "days": 42,
+      "completed": false,
+      "selected": false,
+      "icon": Icons.fitness_center,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +50,13 @@ class HabitsPage extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {},
-            icon:Icon(Icons.notifications_outlined),
-          )
+            icon: Icon(Icons.notifications_outlined),
+          ),
+          if (habits.any((habit) => habit["selected"] == true))
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: deleteSelectedHabits,
+            ),
         ],
       ),
       body: SafeArea(
@@ -41,29 +79,249 @@ class HabitsPage extends StatelessWidget {
                 style: TextStyle(color: Colors.grey, fontSize: 20),
               ),
               const SizedBox(height: 20),
-              Row(children: [
-                SizedBox(
-                  width: 150,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 88, 233, 93),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SizedBox(
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 28, 34, 28),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 78, 101, 78),
+                            width: 1.5,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Total Streak',
+                                style: TextStyle(
+                                  color: Colors.grey[300],
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                '129 ',
+                                style: TextStyle(
+                                  color: Colors.green[400],
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                    child: const Text(
-                      'Add Habit',
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                    ),
+                      const SizedBox(width: 20),
+                      //Completion Rate Card
+                      SizedBox(
+                        width: 165,
+                        height: 120,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 28, 34, 28),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 78, 101, 78),
+                              width: 1.5,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Completion',
+                                  style: TextStyle(
+                                    color: Colors.grey[300],
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  '67%',
+                                  style: TextStyle(
+                                    color: Colors.green[400],
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 165,
+                        height: 120,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 28, 34, 28),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 78, 101, 78),
+                              width: 1.5,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'XP Earned',
+                                  style: TextStyle(
+                                    color: Colors.grey[300],
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  '10K ',
+                                  style: TextStyle(
+                                    color: Colors.green[400],
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],)
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: habits.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onLongPress: () {
+                        setState(() {
+                          habits[index]['selected'] =
+                              !habits[index]['selected'];
+                        });
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 15),
+                        padding: const EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          color: habits[index]["selected"]
+                              ? Colors.green.withOpacity(0.2)
+                              : const Color(0xff1D2128),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        child: ListTile(
+                          leading: Icon(
+                            habits[index]["icon"],
+                            color: Colors.green,
+                          ),
+                          title: Text(
+                            habits[index]["name"],
+                            style: const TextStyle(color: Colors.white),
+                          ),
+
+                          subtitle: Text(
+                            "${habits[index]["days"]} days",
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+
+                          trailing: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                habits[index]["completed"] =
+                                    !habits[index]["completed"];
+                              });
+                            },
+                            child: Icon(
+                              Icons.check_circle,
+                              color: habits[index]["completed"]
+                                  ? Colors.green
+                                  : Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: addHabit,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void deleteSelectedHabits() {
+    setState(() {
+      habits.removeWhere((habit) => habit["selected"] == true);
+    });
+  }
+
+  void addHabit() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Add Habit"),
+          content: TextField(controller: controller),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                controller.clear();
+              },
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (controller.text.trim().isEmpty) {
+                  return;
+                }
+
+                setState(() {
+                  habits.add({
+                    "name": controller.text,
+                    "days": 0,
+                    "completed": false,
+                    "selected": false,
+                    "icon": Icons.task_alt,
+                  });
+                });
+
+                controller.clear();
+                Navigator.pop(context);
+              },
+              child: const Text("Add"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
